3d62615e3767777fea8800377fe177e37cbc44b0
smabari <mdazizulbarishafin@gmail.com>
2025-09-06 10:16:16 +0600

    Refactor data type declarations for clarity and organization

--- module1/src/1.2.ts
+++ module1/src/1.2.ts
@@ -1,1 +1,21 @@
 // basic data type
+
+// string
+let firstName: string = "mezba";
+// number
+let roll: number = 123;
+// boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+let y: null = null;
+
+// array
+let friends: string[] = ["racel", "monica"];
+let eligibleRollList: number[] = [1, 3, 6];
+eligibleRollList.push(32);
+
+// tuple => array => order => type of values
+let coordinates: [number, number] = [1, 5];
+let ageName: [number, string, boolean] = [50, "Mr.X", true];
+
